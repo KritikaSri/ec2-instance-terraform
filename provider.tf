326372d99df0e1671922ee2e65f66615b9c15d88
KritikaSri <51196851+KritikaSri@users.noreply.github.com>
2024-01-06 12:25:59 -0700

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+  access_key = "AKIA6ODUZP55KS6RW7VW"
+  secret_key = "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"
+  region     = "us-west-2"
+}
 resource "aws_instance" "test" {
   ami = "ami-008fe2fc65df48dac"
   instance_type = "t2.micro"
