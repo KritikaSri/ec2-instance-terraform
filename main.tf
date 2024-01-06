62be3070f5641e2657dd887002a30bab823539d2
KritikaSri <51196851+KritikaSri@users.noreply.github.com>
2024-01-06 10:39:24 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,32 +1,10 @@
-/* Sample terraform script to create an EC2 instance
- * Contact : www.arshadzackeriya.com, hello@arshadzackeriya.com
- * Written by Arshad Zackeriya, May 2018
- */
-
 provider "aws" {
-  region = "${var.region}"
-  shared_credentials_file = "${var.shared_credentials_file}"
-  profile = "${var.profile}"
+  region = "us-east-1"
 }
-
-resource "aws_instance" "web-server" {
-  ami = "${var.ami}"
-  instance_type = "${var.instance_type}"
-  availability_zone = "${var.availability_zone}"
-  vpc_security_group_ids        = [
-        "${var.vpc_rds_security_group_id}"
-    ]
-  subnet_id = "${var.subnet_id}"
-  key_name = "${var.key_name}"
-  disable_api_termination = "true"
-
-  root_block_device {
-        delete_on_termination = "true"
-        volume_type = "gp2"
-        volume_size = "${var.volume_size}"
-    }
-
-  tags {
-    Name = "${var.server_name}"
-  }
+resource "aws_instance" "test_instance" {
+ ami           = "ami-008fe2fc65df48dac"
+ instance_type = "t2.micro"
+ tags = {
+   Name = "terraform-test"
+ }
 }
