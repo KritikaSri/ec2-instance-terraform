53bb01e937a6f78a139f7ab1930bf9fa969054e3
KritikaSri <51196851+KritikaSri@users.noreply.github.com>
2024-01-06 12:56:38 -0700

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,8 +3,8 @@ provider "aws" {
   secret_key = "f6hu3RPs9kzDCIp5mSi/I4hMbWJLxFt58GZGcb3Z"
   region     = "us-west-2"
 }
-resource "aws_instance" "test" {
-  ami = "ami-008fe2fc65df48dac"
-  instance_type = "t2.micro"
-}
+#resource "aws_instance" "test" {
+ # ami = "ami-008fe2fc65df48dac"
+  #instance_type = "t2.micro"
+#}
 
